f75d6730c2298329e50799d657386e2236f3c7a3
magcil <pkaliosis@github.com>
2023-12-31 18:39:53 +0200

    added transformer tuner script

--- models/anomaly_transformer.py
+++ models/anomaly_transformer.py
@@ -234,7 +234,7 @@ def vit_small(patch_size=16, **kwargs):
 
 def vit_base(patch_size=16, **kwargs):
     model = VisionTransformer(
-        patch_size=patch_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4,
+        patch_size=patch_size, embed_dim=768, num_heads=12, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
