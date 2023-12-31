2b6466d679c32297fbb46b6e1b6e1d672c87dd14
Christos Nikou <chrisnick92@gmail.com>
2023-12-31 21:34:45 +0200

    Allow to choose model & svm/mse metric for each patient

--- src/end_to_end_evaluation.py
+++ src/end_to_end_evaluation.py
@@ -55,7 +55,6 @@ if __name__ == '__main__':
     patients = json_config["patients"]
     window_size = json_config["window_size"]
     upsampling_size = json_config["upsampling_size"]
-    models = json_config["models"]
     path_of_pt_files = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))),
                                     json_config['pretrained_models'])
 
@@ -131,118 +130,144 @@ if __name__ == '__main__':
         val_dset._upsample_data(upsample_size=upsampling_size)
 
         # Train and validate for each model
-        for model_str in tqdm(models, desc="Validating model", leave=False):
-            model = get_model(model_str=json_config["patients_config"][f"P{patient_id}"]["model"])
+        model_str = json_config["patients_config"][f"P{patient_id}"]["model"]
+        model = get_model(model_str=json_config["patients_config"][f"P{patient_id}"]["model"])
 
-            # Get patient's path to store pt files
-            pt_file = os.path.join(
-                path_of_pt_files,
-                f"Track_{track_id}_P{patient_id}_" + model_str + "_" + str(datetime.today().date()) + ".pt")
+        # Get patient's path to store pt files
+        pt_file = os.path.join(
+            path_of_pt_files,
+            f"Track_{track_id}_P{patient_id}_" + model_str + "_" + str(datetime.today().date()) + ".pt")
 
-            # Start training
-            rec_loss_train = autoencoder_train_loop(train_dset=train_dset,
-                                                    val_dset=val_dset,
-                                                    model=model,
-                                                    epochs=json_config["epochs"],
-                                                    batch_size=json_config["batch_size"],
-                                                    patience=json_config["patience"],
-                                                    learning_rate=json_config["learning_rate"],
-                                                    pt_file=pt_file,
-                                                    device=device,
-                                                    num_workers=json_config['num_workers'])
+        # Start training
+        rec_loss_train = autoencoder_train_loop(train_dset=train_dset,
+                                                val_dset=val_dset,
+                                                model=model,
+                                                epochs=json_config["epochs"],
+                                                batch_size=json_config["batch_size"],
+                                                patience=json_config["patience"],
+                                                learning_rate=json_config["learning_rate"],
+                                                pt_file=pt_file,
+                                                device=device,
+                                                num_workers=json_config['num_workers'])
 
-            # Load best model and validate
-            model.load_state_dict(torch.load(pt_file, map_location=device))
-            # Re-initialize training set to fit distribution on losses
-            train_dset = PatientDataset(track_id=track_id,
+        # Load best model and validate
+        model.load_state_dict(torch.load(pt_file, map_location=device))
+        # Re-initialize training set to fit distribution on losses
+        train_dset = PatientDataset(track_id=track_id,
+                                    patient_id=patient_id,
+                                    mode="train",
+                                    window_size=window_size,
+                                    extension=json_config["file_format"],
+                                    feature_mapping=feature_mapping,
+                                    from_path=True)
+        train_dset.mean, train_dset.std = mu, std
+
+        # Update results
+        results['Total days on train'].append(len(train_dset))
+        results['Model'].append(model_str)
+        results['Patient_id'].append(patient_id)
+
+        # Upsample on predictions
+        train_dset._upsample_data(upsample_size=json_config["prediction_upsampling"])
+        test_dset._upsample_data(upsample_size=json_config["prediction_upsampling"])
+
+        val_results = validation_loop(
+            train_dset=train_dset,
+            test_dset=test_dset,
+            model=model,
+            one_class_test=json_config['patients_config'][f"P{patient_id}"]['one_class_test'],
+            device=device)
+
+        results['one_class_test'].append(json_config['patients_config'][f"P{patient_id}"]['one_class_test'])
+
+        anomaly_scores, labels = val_results['anomaly_scores'], val_results['labels']
+
+        if "postprocessing_filters" in json_config.keys():
+            filter_scores = {}
+            for filter_size in json_config["postprocessing_filters"]:
+                filter_scores[f'median filter scores ({filter_size})'] = []
+                filter_scores[f'mean filter scores ({filter_size})'] = []
+
+        # Write csvs with predictions
+        patient_path = parser.get_path(track_id, patient_id)
+        df = pd.DataFrame({
+            "anomaly_scores": anomaly_scores,
+            "label": labels,
+            "split_day": val_results["split_days"]
+        })
+        df['split'] = [x.split("_")[0] + "_" + str(x.split("_")[1]) for x in df['split_day']]
+        df['day_index'] = [int(x.split("_")[3]) for x in df["split_day"]]
+
+        anomaly_scores, labels = [], []
+
+        for sp in df['split'].unique():
+            mode, num = sp.split("_")[0], int(sp.split("_")[1])
+            filt_df = df[df['split'] == sp]
+
+            filt_df = fill_predictions(track_id=track_id,
                                         patient_id=patient_id,
-                                        mode="train",
-                                        window_size=window_size,
-                                        extension=json_config["file_format"],
-                                        feature_mapping=feature_mapping,
-                                        from_path=True)
-            train_dset.mean, train_dset.std = mu, std
+                                        anomaly_scores=filt_df['anomaly_scores'],
+                                        split=sp,
+                                        days=filt_df['day_index'])
 
-            # Update results
-            results['Total days on train'].append(len(train_dset))
-            results['Model'].append(model_str)
-            results['Patient_id'].append(patient_id)
+            path_to_save = parser.get_path(track=track_id, patient=patient_id, mode=mode, num=num)
+            filt_df.to_csv(os.path.join(path_to_save, f"results_{model_str}_{datetime.today().date()}.csv"))
 
-            # Upsample on predictions
-            train_dset._upsample_data(upsample_size=json_config["prediction_upsampling"])
-            test_dset._upsample_data(upsample_size=json_config["prediction_upsampling"])
+            anomaly_scores_temp = filt_df['anomaly_scores'].to_numpy()
 
-            val_results = validation_loop(
-                train_dset=train_dset,
-                test_dset=test_dset,
-                model=model,
-                one_class_test=json_config['patients_config'][f"P{patient_id}"]['one_class_test'],
-                device=device)
-
-            results['one_class_test'].append(json_config['patients_config'][f"P{patient_id}"]['one_class_test'])
-
-            anomaly_scores, labels = val_results['anomaly_scores'], val_results['labels']
-
-            # Write csvs with predictions
-            patient_path = parser.get_path(track_id, patient_id)
-            df = pd.DataFrame({
-                "anomaly_scores": anomaly_scores,
-                "label": labels,
-                "split_day": val_results["split_days"]
-            })
-            df['split'] = [x.split("_")[0] + "_" + str(x.split("_")[1]) for x in df['split_day']]
-            df['day_index'] = [int(x.split("_")[3]) for x in df["split_day"]]
-
-            anomaly_scores, labels = [], []
-
-            for sp in df['split'].unique():
-                mode, num = sp.split("_")[0], int(sp.split("_")[1])
-                filt_df = df[df['split'] == sp]
-
-                filt_df = fill_predictions(track_id=track_id,
-                                           patient_id=patient_id,
-                                           anomaly_scores=filt_df['anomaly_scores'],
-                                           split=sp,
-                                           days=filt_df['day_index'])
-
-                path_to_save = parser.get_path(track=track_id, patient=patient_id, mode=mode, num=num)
-                filt_df.to_csv(os.path.join(path_to_save, f"results_{model_str}_{datetime.today().date()}.csv"))
-
-                anomaly_scores.append(filt_df['anomaly_scores'].to_numpy())
-                labels.append(filt_df['relapse'].to_numpy())
-
-            anomaly_scores, labels = np.concatenate(anomaly_scores), np.concatenate(labels)
-            anomaly_scores_random = np.repeat(0.5, len(anomaly_scores))
-
-            scores = calculate_roc_pr_auc(anomaly_scores, labels)
-
-            results["ROC AUC"].append(scores["ROC AUC"])
-            results['PR AUC'].append(scores["PR AUC"])
-
-            # Compute metrics for random guess
-            scores = calculate_roc_pr_auc(anomaly_scores_random, labels)
-            results["ROC AUC (random)"].append(scores["ROC AUC"])
-            results['PR AUC (random)'].append(scores["PR AUC"])
-
-            results['Total days (non relapsed)'].append(len(labels[labels == 0]))
-            results['Total days (relapsed)'].append(len(labels[labels == 1]))
-
-            # Use Median/Mean smoothing
             if "postprocessing_filters" in json_config.keys():
                 for filter_size in json_config["postprocessing_filters"]:
-                    # Use median filtering
-                    median_anomaly_scores = medfilt(anomaly_scores, filter_size)
-                    # Get scores and append
-                    scores = calculate_roc_pr_auc(median_anomaly_scores, labels)
-                    results[f"median filter ROC AUC ({filter_size})"].append(scores["ROC AUC"])
-                    results[f"median filter PR AUC ({filter_size})"].append(scores["PR AUC"])
+                    # Median filter
+                    median_anomaly_scores = medfilt(anomaly_scores_temp, filter_size)
+                    filter_scores[f'median filter scores ({filter_size})'].append(median_anomaly_scores)
 
-                    # Use mean filtering
-                    mean_anomaly_scores = np.convolve(anomaly_scores, np.ones(filter_size) / filter_size, "same")
-                    scores = calculate_roc_pr_auc(mean_anomaly_scores, labels)
-                    results[f"mean filter ROC AUC ({filter_size})"].append(scores["ROC AUC"])
-                    results[f"mean filter PR AUC ({filter_size})"].append(scores["PR AUC"])
+                    # Mean filter
+                    mean_anomaly_scores = np.convolve(anomaly_scores_temp,
+                                                        np.ones(filter_size) / filter_size, "same")
 
-            # Write csvs
-            final_df = pd.DataFrame(results)
-            final_df.to_csv("results_" + str(datetime.today().date()) + "upsampling_120_bs128_ws32_depth12_fewer.csv")
+                    filter_scores[f'mean filter scores ({filter_size})'].append(mean_anomaly_scores)
+
+                    filt_df[f'median filter scores ({filter_size})'] = median_anomaly_scores
+                    filt_df[f'mean filter scores ({filter_size})'] = mean_anomaly_scores
+
+
+            filt_df.to_csv(os.path.join(path_to_save, f"results_{model_str}_{datetime.today().date()}.csv"))
+
+            anomaly_scores.append(anomaly_scores_temp)
+            labels.append(filt_df['relapse'].to_numpy())
+
+        anomaly_scores, labels = np.concatenate(anomaly_scores), np.concatenate(labels)
+        anomaly_scores_random = np.repeat(0.5, len(anomaly_scores))
+
+        scores = calculate_roc_pr_auc(anomaly_scores, labels)
+
+        results["ROC AUC"].append(scores["ROC AUC"])
+        results['PR AUC'].append(scores["PR AUC"])
+
+        # Compute metrics for random guess
+        scores = calculate_roc_pr_auc(anomaly_scores_random, labels)
+        results["ROC AUC (random)"].append(scores["ROC AUC"])
+        results['PR AUC (random)'].append(scores["PR AUC"])
+
+        results['Total days (non relapsed)'].append(len(labels[labels == 0]))
+        results['Total days (relapsed)'].append(len(labels[labels == 1]))
+
+        # Use Median/Mean smoothing
+        if "postprocessing_filters" in json_config.keys():
+            for filter_size in json_config["postprocessing_filters"]:
+                # Use median filtering
+                median_anomaly_scores = np.concatenate(filter_scores[f'median filter scores ({filter_size})'])
+                # Get scores and append
+                scores = calculate_roc_pr_auc(median_anomaly_scores, labels)
+                results[f"median filter ROC AUC ({filter_size})"].append(scores["ROC AUC"])
+                results[f"median filter PR AUC ({filter_size})"].append(scores["PR AUC"])
+
+                # Use mean filtering
+                mean_anomaly_scores = np.concatenate(filter_scores[f'mean filter scores ({filter_size})'])
+                scores = calculate_roc_pr_auc(mean_anomaly_scores, labels)
+                results[f"mean filter ROC AUC ({filter_size})"].append(scores["ROC AUC"])
+                results[f"mean filter PR AUC ({filter_size})"].append(scores["PR AUC"])
+
+        # Write csvs
+        final_df = pd.DataFrame(results)
+        final_df.to_csv("results_" + str(datetime.today().date()) + "upsampling_120_bs128_ws32_depth12_fewer.csv")
