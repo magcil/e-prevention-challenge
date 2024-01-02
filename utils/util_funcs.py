1c50a60e4ce5ec0f87cb6b9fce3b4f1a2afc2ca3
Christos Nikou <chrisnick92@gmail.com>
2024-01-02 20:52:02 +0200

    [Add] submission script

--- utils/util_funcs.py
+++ utils/util_funcs.py
@@ -7,6 +7,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pandas as pd
 import numpy as np
 from sklearn.metrics import precision_recall_curve, roc_curve, auc
+from scipy.signal import medfilt
 
 from utils import parse
 
@@ -96,3 +97,21 @@ def calculate_roc_pr_auc(anomaly_scores, labels):
     fpr, tpr, _ = roc_curve(labels, anomaly_scores)
 
     return {"ROC AUC": auc(fpr, tpr), "PR AUC": auc(recall, precision)}
+
+
+def svm_score(preds, dist_from_hp):
+    if len(dist_from_hp) > 1:
+        min_, max_ = dist_from_hp.min(), dist_from_hp.max()
+        median = 1 if max_ == min_ else ((dist_from_hp.median() - min_) / (max_ - min_)) + 1
+    else:
+        median = 1
+    score = median * (preds[preds == -1].shape[0] / len(preds))
+
+    return min(score, 1)
+
+
+def apply_postprocessing_filter(anomaly_scores, filter_type, filter_size):
+    if filter_type == 'median':
+        return medfilt(anomaly_scores, filter_size)
+    elif filter_type == 'mean':
+        return np.convolve(anomaly_scores, np.ones(filter_size) / filter_size)
